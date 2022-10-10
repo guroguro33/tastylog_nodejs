88578d16831a56d88c76dd6b93d3a74b8988bb42
Toru <t02f447f@gmail.com>
2022-10-10 13:46:04 +0900

    Add serve-favicon

--- app.js
+++ app.js
@@ -2,12 +2,15 @@ const PORT = process.env.PORT || 3000;
 // pathモジュールを読み込む プロジェクトの様々なpathを取得するメソッドが使える
 const path = require("path");
 const express = require("express");
+const favicon = require("serve-favicon");
 const app = express();
 
 // Express setting 
 app.set("view engine", "ejs"); // お約束
 
 // Static resource rooting
+// ファビコン
+app.use(favicon(path.join(__dirname, "/public/favicon.ico")));
 app.use("/public", express.static(path.join(__dirname, "/public"))); // __dirnameはnode.jsで使用できる絶対パスを取得する特別な変数
 
 // Dynamic resource rooting.
