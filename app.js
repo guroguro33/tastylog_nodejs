93aaa900a1f0d6a05dbb0c49ad18b303383d84e2
Toru <t02f447f@gmail.com>
2022-11-06 07:50:25 +0900

    Add log4js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const PORT = process.env.PORT || 3000;
 // pathモジュールを読み込む プロジェクトの様々なpathを取得するメソッドが使える
 const path = require("path");
+const logger = require("./lib/log/logger.js");
 const express = require("express");
 const favicon = require("serve-favicon");
 const app = express();
@@ -20,5 +21,5 @@ app.use("/", require("./routes/index.js"));
 
 // Execute web application.
 app.listen(PORT, () => {
-  console.log(`Application listening at :${PORT}`);
+  logger.console.info(`Application listening at :${PORT}`);
 });
