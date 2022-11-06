cce5f37422c47defd41d4b33e135830727a2562f
Toru <t02f447f@gmail.com>
2022-11-07 05:54:59 +0900

    Add application log

--- config/log4js.config.js
+++ config/log4js.config.js
@@ -1,8 +1,17 @@
+const path = require("path");
+const LOG_ROOT_DIR = process.env.LOG_ROOT_DIR || path.join(__dirname, "../logs");
+
 module.exports = {
   appenders: {
     // 任意の名称をつける
     ConsoleLogAppender: {
       type: "console"
+    },
+    ApplicationLogAppender: {
+      type: "dateFile", // 日ファイル
+      filename: path.join(LOG_ROOT_DIR, "./application.log"),
+      pattern: "yyyyMMdd",
+      numBackups: 7 // 7日間保持
     }
   },
   categories: {
@@ -10,6 +19,14 @@ module.exports = {
     "default": {
       appenders: ["ConsoleLogAppender"], // appendersの任意名称を指定
       level: "ALL"
+    },
+    // 個別設定を追加
+    "application": {
+      appenders: [
+        "ApplicationLogAppender", // ログファイル出力設定
+        "ConsoleLogAppender" // コンソール出力の設定
+      ],
+      level: "INFO"
     }
   }
 };
