7c92d83abefc1f2cd01523b02cebd37b5d39d806
Toru <t02f447f@gmail.com>
2022-12-24 22:08:33 +0900

    Add member home view

--- routes/account.js
+++ routes/account.js
@@ -1,6 +1,11 @@
 const router = require("express").Router();
 const {authenticate, authorize, PRIVILEGE} = require("../lib/security/accesscontrol.js");
 
+// 会員画面
+router.get("/", (req, res) => {
+  res.render("./account/index.ejs");
+});
+
 router.get("/login", (req, res) => {
   // ログインエラー用にflashを渡しておく
   res.render("./account/login.ejs", {message: req.flash("message")});
