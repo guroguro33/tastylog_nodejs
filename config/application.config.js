79465532d3768d6b3df999a8aa033c07b38602b2
Toru <t02f447f@gmail.com>
2022-12-28 06:02:19 +0900

    Add login history

--- config/application.config.js
+++ config/application.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   PORT: process.env.PORT || 3000,
   security: {
-    SESSION_SECRET: "YOUR_SESSION_SECRET_STRING"
+    SESSION_SECRET: "YOUR_SESSION_SECRET_STRING",
+    ACCOUNT_LOCK_WINDOW: 30,
+    ACCOUNT_LOCK_THRESHOLD: 5,
+    ACCOUNT_LOCK_TIME: 30,
+    MAX_LOGIN_HISTORY: 20
   },
   search: {
     // 検索時のページあたりの最大表示数
